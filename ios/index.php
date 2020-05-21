fe3fb666f9fbd02bd021b66c35f9a188db1abc4b
Pattern <lyuboslav.yosifov@gmail.com>
2020-05-21 11:04:36 +0300

    Initial commit

---
+++ ios/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+
+    <?php 
+$iphone = strpos($_SERVER['HTTP_USER_AGENT'],”iPhone”);
+$android = strpos($_SERVER['HTTP_USER_AGENT'],”Android”); 
+$palmpre = strpos($_SERVER['HTTP_USER_AGENT'],”webOS”);
+$ipod = strpos($_SERVER['HTTP_USER_AGENT'],”iPod”); 
+if($iphone) { ?>
+    <link rel="stylesheet" type="text/css" href="style_ios.css">
+<?php }  else if ($android) { ?>
+    <link rel="stylesheet" type="text/css" href="style.css">
+<?php } ?>
+
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="main.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display&display=swap" rel="stylesheet">
+    <meta property="og:image" content="https://flic.kr/p/2iCzmSG" /> 
+    <title>Fotowizard Bulgaria</title>
+
+
+</head>
+<body>
+
+    <div class="pimg1">
+  
+</div>
+
+<div class="pimg1-mob">
+
+</div>
+
+
+<section class="section-light">
+    <a href="indexbg.html"><img src="Images/bulgaria.png" class="icon"></a>
+    <h1>Welcome to Fotowizard Bulgaria</h1>
+    <p>If you keep scrolling you'll find more of my pictures, contacts and prices.</p>
+</section>
+
+
+<div class="pimg2">
+  
+</div>
+
+
+<div class="pimg2-mob">
+  
+</div>
+
+<section class="section-dark">
+    <h1 style="text-align: center;">If you wish to see more of my photos...</h1>
+    <p style="text-align: center;">This link will take you to my Flickr gallery where you can view all my photos in full quality.</p>
+    <a href="https://www.flickr.com/photos/187323941@N04/" class="myButton" target="_blank">View my photos</a>
+    
+
+</section>
+
+
+
+
+<div class="pimg3">
+ 
+</div>
+
+
+<div class="pimg3-mob">
+ 
+</div>
+
+
+<section class="section-dark">
+    <h1>If you want your own photoshoot...</h1>
+    <p>You can find more information about me, my contact info and pricing below.
+    </br>
+    </p>
+    <a href="aboutme.html" class="myButton" target="_blank">Contacts</a>
+    <a href="prices.html" class="myButton" target="_blank">Prices</a>
+</section>
+
+
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,4 @@
+if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPad/i))) {
+    location.replace("http://fotowizard.one/aboutme");
+ }
+
